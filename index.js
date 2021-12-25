cc130b6a4c7bcbcccbebc4e0c41fc89d581e60a2
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-12-25 23:01:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,46 @@ const input = $("#i");
 const button = $("#s");
 const q = new URLSearchParams(location.search.substring(1)).get("q");
 
+window.cp = (function(window, document, navigator) {
+  let textArea, copy;
+
+  function isOS() {
+    return navigator.userAgent.match(/ipad|iphone/i);
+  }
+  function createTextArea(text) {
+    textArea = document.createElement('textArea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+  }
+  function selectText() {
+    let range, selection;
+    if (isOS()) {
+      range = document.createRange();
+      range.selectNodeContents(textArea);
+      selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      textArea.setSelectionRange(0, 999999);
+    } else {
+      textArea.select();
+    }
+  }
+  function copyToClipboard() {        
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+  }
+  copy = async function(text) {
+    createTextArea(text);
+    selectText();
+    copyToClipboard();
+  };
+  return {
+    copy
+  }
+})(window, document, navigator);
+
 button.addEventListener("click", async () => {
-  const r = await copy(
+  const r = await cp.copy(
     await generate(input.value)).then(() => alert("クリップボードにコピーしたわぼけ")).catch((e) => alert("エラーだわぼけ" + e.message)
   );
   history.replaceState(null, null, "index.html?q=" + input.value);
@@ -23,9 +61,8 @@ async function generate(string) {
   return string;
 }
 
-async function copy(e){const n=document.createElement("textarea");n.value=e,document.body.appendChild(n),n.select(),document.execCommand("copy"),n.parentElement.removeChild(n)}
-
 if (q) {
   input.value = q;
   button.click();
 }
+
