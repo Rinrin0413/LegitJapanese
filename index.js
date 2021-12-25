3cf4a8015ec1f77fd7e3b9e0b7a1174bf8df36e0
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-12-25 19:01:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ button.addEventListener("click", async () => {
 });
 
 async function generate(string) {
-  const { decode } = new TextDecoder("iso-8859-1");
+  const { decode } = new TextDecoder("utf-8");
   const src = await fetch(
     "https://raw.githubusercontent.com/Rinrin0413/MS.dic_Ayashiy-Nipongo/master/assets/MS-IME-DICT_Ayashiy-Nipongo_v1.1.0.txt",
   ).then(async r => r.arrayBuffer());
