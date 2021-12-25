165aabc9836c6cf019d6c09aae03d70cf3867fd6
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-12-25 18:03:33 +0900

    ni

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const { q } = [...new URLSearchParams(Location.search.substring(1)).values());
 if (q) input.value = q;
 
 button.addEventListener("click", async () => {
-  const r = await navigator.clipboard.writeText(generate(input.value)).then(() => alert("クリップボードにコピーしたわぼけ")).catch(() => alert("エラーだわぼけ"));
+  const r = await navigator.clipboard.writeText(
+    await generate(input.value)).then(() => alert("クリップボードにコピーしたわぼけ")).catch(() => alert("エラーだわぼけ")
+  );
   history.replaceState({ q: input.value }, null, "index.html");
 });
 
+async function generate(string) {
+  const src = await fetch(
+    "https://raw.githubusercontent.com/Rinrin0413/MS.dic_Ayashiy-Nipongo/master/assets/MS-IME-DICT_Ayashiy-Nipongo_v1.1.0.txt",
+  ).then(async r => r.text());
+  const { json } = MSIMEDict(src);
+  return json();
+}
